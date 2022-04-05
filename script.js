6f3202a042baa5a929807f83641f19e54260f61a
Tavonga <67345561+iknowtavonga@users.noreply.github.com>
2022-04-05 20:26:02 +0200

    how to define and call a function

---
+++ script.js
@@ -0,0 +1,9 @@
+// This is how you define a function
+function singSong() {
+    console.log('do');
+    console.log('re');
+    console.log('mi');
+}
+
+// After defining a function, this is how you call the function
+singSong();
