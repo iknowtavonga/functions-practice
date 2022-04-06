5b1a9b2d02c743add59df39f38ee86283909a6f6
Tavonga <67345561+iknowtavonga@users.noreply.github.com>
2022-04-06 19:55:50 +0200

    concatinating a string n number of times

--- script.js
+++ script.js
@@ -21,3 +21,12 @@ function introduction(firstName, lastName) {
 }
 // Calling multiple arguments
 introduction('Tavonga', 'Karimanzira')
+
+// concatinating a string n number of times
+function repeat(str, numTimes) {
+    let result = '';
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
